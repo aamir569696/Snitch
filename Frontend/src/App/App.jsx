7ef853cecc9b35e8ed6d77ba73cbdbc2a312fabf
Muhammad Amir <aamir569696@gmail.com>
2026-09-13 01:02:59 +0500

    Add react-router for routing and set up initial route

--- Frontend/src/App/App.jsx
+++ Frontend/src/App/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { RouterProvider } from 'react-router'
+import { router } from './app.route'
 
 const App = () => {
   return (
     <>
-  
+      <RouterProvider router={router} />
+
     </>
   )
 }
